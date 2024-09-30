aabb0c11e7a8bbe54c6ab272df986ac7c27e998b
cjp_jx <cjp_jx@outlook.com>
2024-09-30 13:42:28 +0800

    移除不完美的深色模式、标签选择和 Github 链接

--- ui/website/src/components/Content/index.tsx
+++ ui/website/src/components/Content/index.tsx
@@ -5,10 +5,7 @@ import { Loading } from "../Loading";
 import { Helmet } from "react-helmet";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import FetchList from "../../utils/api";
-import TagSelector from "../TagSelector";
 import pinyin from "pinyin-match";
-import GithubLink from "../GithubLink";
-import DarkSwitch from "../DarkSwitch";
 import { isLogin } from "../../utils/check";
 import { generateSearchEngineCard } from "../../utils/serachEngine";
 import { toggleJumpTarget } from "../../utils/setting";
@@ -24,65 +21,39 @@ const mutiSearch = (s, t) => {
 const Content = (props: any) => {
   const [data, setData] = useState<any>({});
   const [loading, setLoading] = useState<boolean>(true);
-  const [currTag, setCurrTag] = useState("全部工具");
   const [searchString, setSearchString] = useState("");
   const [val, setVal] = useState("");
 
   const filteredDataRef = useRef<any>([]);
 
-  const showGithub = useMemo(() => {
-    const hide = data?.setting?.hideGithub === true
-    return !hide;
-  }, [data])
   const loadData = useCallback(async () => {
     try {
       setLoading(true);
       const r = await FetchList();
       setData(r);
-      const tagInLocalStorage = window.localStorage.getItem("tag");
-      if (tagInLocalStorage && tagInLocalStorage !== "") {
-        if (r?.catelogs && r?.catelogs.includes(tagInLocalStorage)) {
-          setCurrTag(tagInLocalStorage);
-        }
-      }
     } catch (e) {
       console.log(e);
     } finally {
       setLoading(false);
     }
-  }, [setData, setLoading, setCurrTag]);
+  }, [setData, setLoading]);
+  
   useEffect(() => {
     loadData();
   }, [loadData]);
 
-  const handleSetCurrTag = (tag: string) => {
-    setCurrTag(tag);
-    // 管理后台不记录了
-    if (tag !== "管理后台") {
-      window.localStorage.setItem("tag", tag);
-    }
-    resetSearch(true);
-  };
-
-  const resetSearch = (notSetTag?: boolean) => {
+  const resetSearch = () => {
     setVal("");
     setSearchString("");
-    const tagInLocalStorage = window.localStorage.getItem("tag");
-    if (!notSetTag && tagInLocalStorage && tagInLocalStorage !== "" && tagInLocalStorage !== "管理后台") {
-      setCurrTag(tagInLocalStorage);
-    }
   };
 
   const handleSetSearch = (val: string) => {
     if (val !== "" && val) {
-      setCurrTag("全部工具");
       setSearchString(val.trim());
     } else {
       resetSearch();
     }
-  }
-
-
+  };
 
   const filteredData = useMemo(() => {
     if (data.tools) {
@@ -94,12 +65,6 @@ const Content = (props: any) => {
           return false;
         })
         .filter((item: any) => {
-          if (currTag === "全部工具") {
-            return true;
-          }
-          return item.catelog === currTag;
-        })
-        .filter((item: any) => {
           if (searchString === "") {
             return true;
           }
@@ -109,15 +74,15 @@ const Content = (props: any) => {
             mutiSearch(item.url, searchString)
           );
         });
-      return [...localResult, ...generateSearchEngineCard(searchString)]
+      return [...localResult, ...generateSearchEngineCard(searchString)];
     } else {
       return [...generateSearchEngineCard(searchString)];
     }
-  }, [data, currTag, searchString]);
+  }, [data, searchString]);
 
   useEffect(() => {
-    filteredDataRef.current = filteredData
-  }, [filteredData])
+    filteredDataRef.current = filteredData;
+  }, [filteredData]);
 
   useEffect(() => {
     if (searchString.trim() === "") {
@@ -127,9 +92,8 @@ const Content = (props: any) => {
     }
     return () => {
       document.removeEventListener("keydown", onKeyEnter);
-    }
-    // eslint-disable-next-line
-  }, [searchString])
+    };
+  }, [searchString]);
 
   const renderCardsV2 = useCallback(() => {
     return filteredData.map((item, index) => {
@@ -140,7 +104,6 @@ const Content = (props: any) => {
           des={item.desc}
           logo={item.logo}
           key={item.id}
-          catelog={item.catelog}
           index={index}
           isSearching={searchString.trim() !== ""}
           onClick={() => {
@@ -153,30 +116,26 @@ const Content = (props: any) => {
         />
       );
     });
-    // eslint-disable-next-line
   }, [filteredData, searchString]);
 
   const onKeyEnter = (ev: KeyboardEvent) => {
     const cards = filteredDataRef.current;
-    // 使用 keyCode 防止与中文输入冲突
     if (ev.keyCode === 13) {
       if (cards && cards.length) {
         window.open(cards[0]?.url, "_blank");
         resetSearch();
       }
     }
-    // 如果按了数字键 + ctrl/meta，打开对应的卡片
     if (ev.ctrlKey || ev.metaKey) {
       const num = Number(ev.key);
       if (isNaN(num)) return;
-      ev.preventDefault()
+      ev.preventDefault();
       const index = Number(ev.key) - 1;
       if (index >= 0 && index < cards.length) {
         window.open(cards[index]?.url, "_blank");
         resetSearch();
       }
     }
-
   };
 
   return (
@@ -200,11 +159,6 @@ const Content = (props: any) => {
               handleSetSearch(t);
             }}
           />
-          <TagSelector
-            tags={data?.catelogs ?? ["全部工具"]}
-            currTag={currTag}
-            onTagChange={handleSetCurrTag}
-          />
         </div>
       </div>
       <div className="content-wraper">
@@ -215,8 +169,6 @@ const Content = (props: any) => {
       <div className="record-wraper">
         <a href="https://beian.miit.gov.cn" target="_blank" rel="noreferrer">{data?.setting?.govRecord ?? ""}</a>
       </div>
-      {showGithub && <GithubLink />}
-      <DarkSwitch showGithub={showGithub} />
     </>
   );
 };
